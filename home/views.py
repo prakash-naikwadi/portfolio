b963ed8b064f8d95ad6938c554c5b9843219b397
Prakash Naikwadi <naikwadiprakash1@gmail.com>
2020-10-07 23:09:39 +0530

    Add styling by boostrap and contact form

--- home/views.py
+++ home/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
 def home(request):
     context = {
-        'subject':'django',
-        'name':'prakash'
+        'subject': 'django',
+        'name': 'prakash'
     }
     return render(request, 'home.html', context)
+
 
 def about(request):
     return render(request, 'about.html')
 
+
 def projects(request):
     return render(request, 'projects.html')
+
 
 def contact(request):
     return render(request, 'contact.html')
