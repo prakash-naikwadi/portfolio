ae8ef3079af26040c2bf2a5f50699d250001d464
Prakash Naikwadi <naikwadiprakash1@gmail.com>
2020-10-07 19:01:53 +0530

    adding templates folder and home.html,about.html,contact.html,projects.html

--- home/views.py
+++ home/views.py
@@ -1,14 +1,18 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("This is home page")
+    context = {
+        'subject':'django',
+        'name':'prakash'
+    }
+    return render(request, 'home.html', context)
 
 def about(request):
-    return HttpResponse("This is about page")
+    return render(request, 'about.html')
 
 def projects(request):
-    return HttpResponse("This is projects page")
+    return render(request, 'projects.html')
 
 def contact(request):
-    return HttpResponse("This is contact page")
+    return render(request, 'contact.html')
