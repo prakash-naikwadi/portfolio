8f99ee5e53a75060aeb1999f1593f888231b78d3
Prakash Naikwadi <naikwadiprakash1@gmail.com>
2020-10-11 15:42:16 +0530

    Created Template Inheritance and base.html is a super template

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from home import views
 
+admin.site.site_header = "Login to Developer Prakash"
+admin.site.site_title = "Welcome To Prakash's Dashbord"
+admin.site.index_title = "Welcome To This Portal"
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('about', views.about, name='about'),
