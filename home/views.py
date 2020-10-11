9fa2ee0f3eea913cb771da13decfaed8dafee42d
Prakash Naikwadi <naikwadiprakash1@gmail.com>
2020-10-11 12:33:39 +0530

    contact model created and contact form data stored in database funtion added

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from home.models import Contact
 
 # Create your views here.
 
@@ -20,4 +21,14 @@ def projects(request):
 
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        desc = request.POST['desc']
+        #print(name, email, phone, desc)
+        ins = Contact(name=name, email=email, phone=phone, desc=desc)
+        ins.save()
+
+
     return render(request, 'contact.html')
